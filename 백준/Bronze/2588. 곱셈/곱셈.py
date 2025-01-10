2afde4c2a8b93bd4af49a6bf94f9408e65702b0f
eda-ginger <rudwls2717@pusan.ac.kr>
2025-01-10 13:02:36 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,11 @@
+a = input()
+b = input()
+
+first = int(b[2]) * int(a)
+second = int(b[1]) * int(a)
+third = int(b[0]) * int(a)
+
+print(first)
+print(second)
+print(third)
+print(first + second * 10 + third * 100)
