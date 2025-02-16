d4702dd5e028dfd114a2ee7c7f3978077c3f8834
eda-ginger <rudwls2717@pusan.ac.kr>
2025-02-16 11:39:44 +0900

    [Bronze II] Title: 애너그램 거리, Time: 164 ms, Memory: 111480 KB -BaekjoonHub

--- 백준/Bronze/3778. 애너그램 거리/애너그램 거리.py
+++ 백준/Bronze/3778. 애너그램 거리/애너그램 거리.py
@@ -1,27 +1,12 @@
-from collections import Counter
-
-def anagram_distance(a, b):
-    count_a = Counter(a)
-    count_b = Counter(b)
-    
-    total_diff = 0
-    
-    # a에 있는 문자 기준으로 차이 계산
-    for char in count_a:
-        total_diff += abs(count_a[char] - count_b.get(char, 0))
-    
-    # b에 있는 문자 기준으로 차이 계산 (이미 처리한 문자는 제외)
-    for char in count_b:
-        if char not in count_a:
-            total_diff += count_b[char]
-    
-    return total_diff
-
-n = int(input())
-for i in range(n):
-    a = input().strip()
-    b = input().strip()
-    
-    result = anagram_distance(a, b)
-    
-    print(f'Case #{i+1}: {result}')
+import sys
+input = sys.stdin.readline
+def solution():
+    for t in range(1,int(input())+1):
+        aa=[0]*26
+        for c in input().strip():
+            aa[ord(c)-97]+=1
+        for c in input().strip():
+            aa[ord(c)-97]-=1
+        aa=map(abs,aa)
+        print(f'Case #{t}: {sum(aa)}')
+solution()
