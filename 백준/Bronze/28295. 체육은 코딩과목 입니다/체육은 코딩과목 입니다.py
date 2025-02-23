89415011dfbc298c2a78dc5e97615ce83608749a
eda-ginger <rudwls2717@pusan.ac.kr>
2025-02-23 16:18:43 +0900

    [Bronze IV] Title: 체육은 코딩과목 입니다, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.py
+++ 백준/Bronze/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.py
@@ -1,8 +1,1 @@
-init = 0
-dn = {1: 90, 2: 180, 3:-90}
-p = {0: 'N', 90: 'E', 180: 'S', 270: 'W'}
-for i in range(10):
-    n = int(input())
-    init += dn[n]
-rn = init % 360
-print(p[rn])
+print("NESW"[sum(map(int,open(0)))%4])
