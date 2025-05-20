bb10a21c2e10190c92de8a1ee674565d281d6e9d
eda-ginger <rudwls2717@pusan.ac.kr>
2025-05-20 10:47:25 +0900

    [Bronze II] Title: 모기곱셈, Time: 132 ms, Memory: 111276 KB -BaekjoonHub

---
+++ 백준/Bronze/6609. 모기곱셈/모기곱셈.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        m,p,l,e,r,s,n=map(int,input().split())
+        for i in range(n):
+            sum=m
+            m=p//s
+            p=l//r
+            l=sum*e
+        print(m)
+    except:
+        break
