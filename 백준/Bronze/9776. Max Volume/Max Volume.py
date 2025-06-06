d6f6b7f28954c264bbd8ddb121050a3a047253af
eda-ginger <rudwls2717@pusan.ac.kr>
2025-06-06 22:32:40 +0900

    [Bronze III] Title: Max Volume, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/9776. Max Volume/Max Volume.py
+++ 백준/Bronze/9776. Max Volume/Max Volume.py
@@ -1,13 +1,13 @@
 n = int(input())
 pie = 3.14159
-vols = []
+vol = []
 for _ in range(n):
     lst = input().split()
     if lst[0] == 'S':
-        vols.append((4/3)*pie*float(lst[1])**3)
+        vol.append((4/3)*pie*float(lst[1])**3)
     elif lst[0] == 'L':
-        vols.append(pie*float(lst[1])**2*float(lst[2]))
+        vol.append(pie*float(lst[1])**2*float(lst[2]))
     else:
-        vols.append((1/3)*pie*float(lst[1])**2*float(lst[2]))
-print(max(vols))
+        vol.append((1/3)*pie*float(lst[1])**2*float(lst[2]))
+print(max(vol))
         
